08f90e9d1c912da7d90f63ec17293ffb1b0397b3
bommumr <bommumr@upmc.edu>
2022-09-10 02:29:37 -0500

    formatting added

--- src/interfaces/advertisement.ts
+++ src/interfaces/advertisement.ts
@@ -1,48 +1,48 @@
 import { Schema, model, Document } from "mongoose";
 
-export interface Iaddress  extends Document {
-    country:Array<String>,
-    streetAddress:String,
-    state:Array<String>
+export interface Iaddress extends Document {
+    country: Array<String>,
+    streetAddress: String,
+    state: Array<String>
     city: Array<String>,
-   zipCode:Number
+    zipCode: Number
 }
-export interface Iproperty  extends Document {
-    builtyear:Number,
-    builtarea:String,
-    bedrooms:Number,
-    halls:Number,
-    bathrooms:Number,
-    floors:Number,
-   zipCode:Number,
-   furnshingStatus:String,
-   Amenties: Array<String>,
-   aroundProperty: Array<String>
+export interface Iproperty extends Document {
+    builtyear: Number,
+    builtarea: String,
+    bedrooms: Number,
+    halls: Number,
+    bathrooms: Number,
+    floors: Number,
+    zipCode: Number,
+    furnshingStatus: String,
+    Amenties: Array<String>,
+    aroundProperty: Array<String>
 }
-export interface Iaddress  extends Document {
-    country:Array<String>,
-    streetAddress:String,
-    state:Array<String>
+export interface Iaddress extends Document {
+    country: Array<String>,
+    streetAddress: String,
+    state: Array<String>
     city: Array<String>,
-   zipCode:Number
+    zipCode: Number
 }
-export interface Icontact  extends Document{
-    role:Array<String>,
-    name:String,
-    phone:Number,
-    email:String
+export interface Icontact extends Document {
+    role: Array<String>,
+    name: String,
+    phone: Number,
+    email: String
 }
 
 export interface Iadvertisement extends Document {
-    category :Array<String>,
-    title:String,
-    price:String
+    category: Array<String>, // it should be an 
+    title: String,
+    price: String
     currencyType: Array<String>,
-    miniumAdvance:String,
-    tags:Array<String>,
-    description:String,
+    miniumAdvance: String,
+    tags: Array<String>,
+    description: String,
     address: Iaddress,
-    images:Array<String>, 
-    property:Iproperty,
-    contact:Icontact
- }
+    images: Array<String>,
+    property: Iproperty,
+    contact: Icontact
+}
--- src/model/Advertisement.ts
+++ src/model/Advertisement.ts
@@ -1,58 +1,58 @@
 import { number, string } from "joi";
 import { Schema, model, Document } from "mongoose";
-import{Iadvertisement} from '../interfaces/advertisement'
+import { Iadvertisement } from '../interfaces/advertisement'
 
-const AddressSchema: Schema = new Schema ({
-     country:{type:Array},
-    streetAddress:{type:String},
-    state:{type:Array},
-    city: {type:Array},
-   zipCode:{type:Number}
-  });
-  const PropertySchema: Schema = new Schema ({
-    builtyear:{type:Number},
-   builtarea:{type:String},
-   bedrooms:{type:Number},
-   halls:{type:Number},
-   bathrooms:{type:Number},
-   floors:{type:Number},
-  zipCode:{type:Number},
-  furnshingStatus:{type:String},
-  Amenties:{type:Array},
-  aroundProperty:{type:Array}
- });
+const AddressSchema: Schema = new Schema({
+  country: { type: Array },
+  streetAddress: { type: String },
+  state: { type: Array },
+  city: { type: Array },
+  zipCode: { type: Number }
+});
+const PropertySchema: Schema = new Schema({
+  builtyear: { type: Number },
+  builtarea: { type: String },
+  bedrooms: { type: Number },
+  halls: { type: Number },
+  bathrooms: { type: Number },
+  floors: { type: Number },
+  zipCode: { type: Number },
+  furnshingStatus: { type: String },
+  Amenties: { type: Array },
+  aroundProperty: { type: Array }
+});
 
- const ContactInfoSchema: Schema = new Schema ({
-  role:{type:Array},
-  name:{type:String},
-  phone:{type:Number},
-  email:{type:String}
+const ContactInfoSchema: Schema = new Schema({
+  role: { type: Array },
+  name: { type: String },
+  phone: { type: Number }, // need isverifed prperty for both email/ phone
+  email: { type: String }
 });
 
 const AdvertisementSchema: Schema = new Schema({
-     category :{type:Array},
-     title:{type:String},
-     price:{type:Number},
-     currencyType:{type:Array},
-     miniumAdvance:{type:Number},
-     tags:{type:Array},
-     description:{type:String},
-     address: {
-        type: AddressSchema,
-        // required: true,
-      },
-      property:{
-        type: PropertySchema,
-        // required: true,
-      },
-      contact:{
-        type:ContactInfoSchema,
-        // required: true,
-      }
+  category: { type: Array },
+  title: { type: String },
+  price: { type: Number },
+  currencyType: { type: Array },
+  miniumAdvance: { type: Number },
+  tags: { type: Array },
+  description: { type: String },
+  address: {
+    type: AddressSchema,
+    // required: true,
+  },
+  property: {
+    type: PropertySchema,
+    // required: true,
+  },
+  contact: {
+    type: ContactInfoSchema,
+    // required: true,
+  }
 
-  });
+});
 
-  export default model<Iadvertisement>("Advertisement", AdvertisementSchema);
+export default model<Iadvertisement>("Advertisement", AdvertisementSchema);
 
 
 
