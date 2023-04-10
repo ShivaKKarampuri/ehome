472ae4c385ba3a3af13217c4d9c5d91d557690d9
Manohar <Manoharreddy0893@gmail.com>
2023-04-10 11:50:53 -0500

    8080 port added

--- src/app.ts
+++ src/app.ts
@@ -33,8 +33,8 @@ app.get('/', (req, res) => {
 app.use(() => {
   throw createHttpError(404, "Route not found");
 });
-app.listen(3000, () => {
-  console.log(`Listening On PORT 3000`);
+app.listen(8080, () => {
+  console.log(`Listening On PORT 8080`);
 });
 
 app.use(errorHandler);
